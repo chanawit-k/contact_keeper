651070696d0f43f6f9ad6d3ef60fa60b10ebccb4
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2022-03-16 01:18:34 +0700

    add state ciontext

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,54 @@
+import React, { useReducer, useContext } from 'react';
+import axios from 'axios';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  GET_CONTACTS,
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_CONTACTS,
+  CLEAR_FILTER,
+  CONTACT_ERROR,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Boom',
+        email: 'boom@mail.com',
+        phone: '123456',
+        type: 'personal',
+      },
+      {
+        id: 2,
+        name: 'Bell',
+        email: 'Bell@mail.com',
+        phone: '5678',
+        type: 'professional',
+      },
+      {
+        id: 3,
+        name: 'New',
+        email: 'new@mail.com',
+        phone: '7898',
+        type: 'friend',
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  return (
+    <ContactContext.Provider value={{ contacts: state.con }}>
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
